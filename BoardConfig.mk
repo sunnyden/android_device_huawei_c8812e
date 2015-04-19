7ec79dfea13ba31b3c92a9f3e7972941213a9fb8
邓昊晴 <exeded@126.com>
2015-04-19 19:36:32 +0800

    change kernel defconfig file

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,7 +34,7 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/huawei/c8812e/bluetooth
 
 
 # Kernel
-TARGET_KERNEL_CONFIG := u8833_defconfig
+TARGET_KERNEL_CONFIG := c8812e_defconfig
 
 # These currently have to go to the ramdisk for wlan_detect to pick them up.
 # Hopefully they can join their friends at $(KERNEL_MODULES_OUT) soon. :(
